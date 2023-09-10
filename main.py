364e93b813b3a67a36b18c0a7c3a1669e3a0c0c5
Robby <crotti.roberto@gmail.com>
2023-09-10 21:13:15 +0200

    added delete all

--- main.py
+++ main.py
@@ -104,6 +104,6 @@ async def delete_fornitura_by_id(id: str, user: dict = Depends(get_current_user)
 async def delete_contratto_by_id(id: str, user: dict = Depends(get_current_user)):
     return db.delete_contratto_by_id(id)
 
-@app.get("/clienti/deleteall", tags=["Get"])
+@app.delete("/clienti/deleteall", tags=["Delete"])
 async def delete_all_clienti(user: dict = Depends(get_current_user)):
     return db.delete_all_clienti()
