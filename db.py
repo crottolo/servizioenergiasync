2ec12163327ce9909f1a4f487f674ff2c63ab0c6
Robby <crotti.roberto@gmail.com>
2023-09-10 20:34:45 +0200

    first commit

---
+++ db.py
@@ -0,0 +1,104 @@
+from pymongo import MongoClient
+import json
+from bson import json_util, ObjectId
+
+# Connettiti all'istanza MongoDB in esecuzione (supponendo che sia in esecuzione sulla porta predefinita 27017 sul tuo localhost)
+client = MongoClient('localhost', 27017)
+
+# Seleziona un database chiamato 'testdb'. Se non esiste, verrà creato.
+db = client.testdb
+
+# Seleziona una collezione chiamata 'people'. Se non esiste, verrà creata.
+clienti = db.clienti
+contratti = db.contratti
+forniture = db.forniture
+
+def insert_cliente(dati_cliente):
+    result = clienti.insert_one(dati_cliente)
+    return result.inserted_id
+
+def insert_contratti(dati_contratto):
+    result = contratti.insert_one(dati_contratto)
+    return result.inserted_id
+
+def insert_forniture(dati_fornitura):
+    result = forniture.insert_one(dati_fornitura)
+    return result.inserted_id
+
+"""
+GET ALL
+"""
+
+def get_clienti():
+    # Converti i risultati in JSON utilizzando json_util di pymongo e poi ritorna come dizionario
+    people_list = list(clienti.find())
+    people_json = json.loads(json_util.dumps(people_list))
+    return people_json
+
+def get_contratti():
+    # Converti i risultati in JSON utilizzando json_util di pymongo e poi ritorna come dizionario
+    contratti_list = list(contratti.find())
+    contratti_json = json.loads(json_util.dumps(contratti_list))
+    return contratti_json
+
+def get_forniture():
+    # Converti i risultati in JSON utilizzando json_util di pymongo e poi ritorna come dizionario
+    fornitura_list = list(forniture.find())
+    fornitura_json = json.loads(json_util.dumps(fornitura_list))
+    return fornitura_json
+
+"""
+Get by ID
+"""
+
+def get_cliente_by_id(id):
+    """Converti i risultati in JSON utilizzando json_util di pymongo e poi ritorna come dizionario"""
+    obj_id = ObjectId(id)
+    people_list = list(clienti.find({"_id": obj_id}))
+    people_json = json.loads(json_util.dumps(people_list))
+    return people_json
+
+def get_fornitura_by_id(id):
+    """Converti i risultati in JSON utilizzando json_util di pymongo e poi ritorna come dizionario"""
+    obj_id = ObjectId(id)
+    fornitura_list = list(forniture.find({"_id": obj_id}))
+    fornitura_json = json.loads(json_util.dumps(fornitura_list))
+    return fornitura_json
+
+def get_contratto_by_id(id):
+    """Converti i risultati in JSON utilizzando json_util di pymongo e poi ritorna come dizionario"""
+    obj_id = ObjectId(id)
+    contratto_list = list(contratti.find({"_id": obj_id}))
+    contratto_json = json.loads(json_util.dumps(contratto_list))
+    return contratto_json
+
+"""
+DELETE BY ID
+"""
+
+def delete_cliente_by_id(id):
+    """Converti i risultati in JSON utilizzando json_util di pymongo e poi ritorna come dizionario"""
+    obj_id = ObjectId(id)
+    delete_result = clienti.delete_one({"_id": obj_id})
+    if delete_result.deleted_count > 0:
+        return True
+    else:
+        return False
+
+def delete_fornitura_by_id(id):
+    """Converti i risultati in JSON utilizzando json_util di pymongo e poi ritorna come dizionario"""
+    obj_id = ObjectId(id)
+    delete_result = forniture.delete_one({"_id": obj_id})
+    if delete_result.deleted_count > 0:
+        return True
+    else:
+        return False
+
+def delete_contratto_by_id(id):
+    """Converti i risultati in JSON utilizzando json_util di pymongo e poi ritorna come dizionario"""
+    obj_id = ObjectId(id)
+    delete_result = contratti.delete_one({"_id": obj_id})
+    if delete_result.deleted_count > 0:
+        return True
+    else:
+        return False
---
+++ main.py
@@ -0,0 +1,106 @@
+from fastapi import FastAPI, Depends, Security, HTTPException
+import typing as t
+from fastapi.security.http import HTTPBearer, HTTPBasicCredentials, HTTPAuthorizationCredentials
+import json
+import db
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+
+
+tags_metadata = [
+    {"name": "Get"},
+    {"name": "Post"},
+    {"name": "Put"},
+    {"name": "Delete"}
+]
+app = FastAPI(openapi_tags=tags_metadata)
+
+secret = os.getenv("TOKEN")
+
+security = HTTPBearer()
+
+def get_current_user(authorization: HTTPAuthorizationCredentials = Depends(security)) -> dict:
+    token = authorization.credentials
+    # Here you should check if the token is valid e.g., using your database.
+    # For this example, I'll assume a token "my-secret-token" is valid.
+    if token == secret:
+        return True
+    raise HTTPException(status_code=403, detail="Invalid token")
+
+
+    
+
+
+
+
+@app.post("/clienti", tags=["Post"])
+async def clienti(data: dict, user: dict = Depends(get_current_user)):
+    
+    if data:
+        db.insert_cliente(data)
+        return {"success": True}
+    else:
+        return {"success": False, "error": "No data"}
+    
+@app.post('forniture', tags=["Post"])
+async def forniture(data: dict, user: dict = Depends(get_current_user)):
+    if data:
+        db.insert_forniture(data)
+        return {"success": True}
+    else:
+        return {"success": False, "error": "No data"}
+    
+    
+@app.post('contratti', tags=["Post"])
+async def contratti(data: dict, user: dict = Depends(get_current_user)):
+    if data:
+        db.insert_contratti(data)
+        return {"success": True}
+    else:
+        return {"success": False, "error": "No data"}
+    
+@app.get("/clienti", tags=["Get"])
+async def get_clienti(user: dict = Depends(get_current_user)):
+   
+    return db.get_clienti()
+
+@app.get("/forniture", tags=["Get"])
+async def get_forniture( user: dict = Depends(get_current_user) ):
+    return db.get_forniture()
+
+@app.get("/contratti", tags=["Get"])
+async def get_contratti( user: dict = Depends(get_current_user)):
+    return db.get_contratti()
+    
+"""GET BY ID"""
+    
+@app.get("/clienti/{id}", tags=["Get"])
+async def get_cliente_by_id(id: str, user: dict = Depends(get_current_user)):
+    return db.get_cliente_by_id(id)
+
+@app.get("/forniture/{id}", tags=["Get"])
+async def get_fornitura_by_id(id: str, user: dict = Depends(get_current_user)):
+    return db.get_fornitura_by_id(id)
+
+@app.get("/contratti/{id}", tags=["Get"])
+async def get_contratto_by_id(id: str, user: dict = Depends(get_current_user)):
+    return db.get_contratto_by_id(id)
+
+"""DELETE BY ID"""
+
+@app.delete("/clienti/{id}", tags=["Delete"])
+async def delete_cliente_by_id(id: str, user: dict = Depends(get_current_user)):
+    return db.delete_cliente_by_id(id)
+
+@app.delete("/forniture/{id}", tags=["Delete"])
+async def delete_fornitura_by_id(id: str, user: dict = Depends(get_current_user)):
+    return db.delete_fornitura_by_id(id)
+
+@app.delete("/contratti/{id}", tags=["Delete"])
+async def delete_contratto_by_id(id: str, user: dict = Depends(get_current_user)):
+    return db.delete_contratto_by_id(id)
+
